3fc9cf1a08e89bb56a5c6e0e1c0e174a09ca0dad
Alexandr <evgdever@gmail.com>
2020-03-02 17:00:12 +0700

    add router and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {useRoutes} from "./router";
+import 'materialize-css';
 
 function App() {
+    const routes = useRoutes(false);
   return (
-      <div>
-          <h1>Hello world!</h1>
-      </div>
+      <Router>
+          <div className="container">
+             {routes}
+          </div>
+      </Router>
   );
 }
 
